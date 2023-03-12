2c4a437403415cb8a875877d6b246f8908abe4b9
tbang-350 <tbway350@gmail.com>
2023-03-12 11:13:01 +0300

    design multi item input with javascript

--- resources/views/backend/purchase/purchase_add.blade.php
+++ resources/views/backend/purchase/purchase_add.blade.php
@@ -86,8 +86,11 @@
                                     <div class="md-3">
                                         <label for="example-text-input" class="form-label" style="margin-top: 43px">
                                         </label>
-                                        <input type="button" value="Add More"
-                                            class="btn btn-secondary btn-rounded waves-effect waves-light">
+                                        
+
+                                        <i class="btn btn-secondary btn-rounded waves-effect waves-light fas fa-plus-circle addeventmore">
+                                            Add More
+                                        </i>
                                     </div>
                                 </div>
 
@@ -99,6 +102,57 @@
                         </div>
                         <!--end cardbody -->
 
+
+                        <div class="card-body">
+
+                            <form action="" method="">
+
+                                @csrf
+
+                                <table class="table-sm table-bordered" width="100%" style="border-color: #ddd">
+
+
+                                    <thead>
+                                        <tr>
+                                            <th>Category</th>
+                                            <th>Product Name</th>
+                                            <th>Unit</th>
+                                            <th>Unit Price</th>
+                                            <th>Description</th>
+                                            <th>Total Price</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody id="addRow" class="addRow">
+
+                                    </tbody>
+
+                                    <tbody>
+                                        <tr>
+                                            <td colspan="5"></td>
+                                            <td>
+                                                <input type="text" name="estimated_amount" value="0"
+                                                    id="estimated_amount" class="form-control estimated_amount" readonly
+                                                    style="background-color: #ddd">
+                                            </td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+
+                                </table>
+
+                                <br>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-info" id="storeButton">Purchase Store</button>
+                                </div>
+
+                            </form>
+
+                        </div>
+
+
                     </div>
 
                 </div> <!-- end col -->
@@ -108,6 +162,96 @@
         </div>
 
     </div>
+
+
+    <script type="text/x-handlebars-template" id="document-template">
+        
+        <tr class="delete_add_more_item" id="delete_add_more_item">
+
+            <input type="hidden" name="date[]" value="@{{date}}">
+            <input type="hidden" name="purchase_no[]" value="@{{purchase_no}}">
+            <input type="hidden" name="supplier_id[]" value="@{{supplier_id}}">
+
+            <td>
+                <input type="hidden" name="category_id[]" value="@{{category_id}}">
+                @{{category_name}}
+            </td>
+
+            <td>
+                <input type="hidden" name="product_id[]" value="@{{product_id}}">
+                @{{product_name}}
+            </td>
+
+            <td>
+                <input type="number" min="1" class="form-control buying_qty text-right" name="buying_qty[]" value="">
+            </td>
+
+            <td>
+                <input type="number" class="form-control unit_price text-right" name="unit_price[]" value="">
+            </td>
+
+            <td>
+                <input type="text" class="form-control" name="description[]" value="">
+            </td>
+
+            <td>
+                <input type="number" class="form-control buying_price text-right" name="buying_price[]" value="0" readonly>
+            </td>
+
+            <td>
+                <i class="btn btn-danger btn-sm fas fa-window-close removeeventmore"></i>
+            </td>
+
+        </tr>
+
+    </script>
+
+
+    <script type="text/javascript">
+
+        $(document).ready(function(){
+            $(document).on("click",".addeventmore",function(){
+
+                var date = $('#date').val();
+                var purchase_no = $('#purchase_no').val();
+                var supplier_id = $('#supplier_id').val();
+                var category_id = $('#category_id').val();
+                var category_name = $('#category_name').find('option:selected').text();
+                var product_id = $('#product_id').val();
+                var product_name = $('#product_name').find('option:selected').text();
+
+                if(date == ''){
+                    $.notify("Date not set" , {globalPosition: 'top-right' ,className: 'error'});
+                    return false;
+                }
+
+                if(purchase_no == ''){
+                    $.notify("Purchase_no  not set" , {globalPosition: 'top-right' ,className: 'error'});
+                    return false;
+                }
+
+                if(supplier_id == ''){
+                    $.notify("Supplier not set" , {globalPosition: 'top-right' ,className: 'error'});
+                    return false;
+                }
+
+                if(category_id == ''){
+                    $.notify("Category not set" , {globalPosition: 'top-right' ,className: 'error'});
+                    return false;
+                }
+
+                if(product_id == ''){
+                    $.notify("Product not set" , {globalPosition: 'top-right' ,className: 'error'});
+                    return false;
+                }
+
+                var source = $("document-template").html();
+                var template = HandleBars.compile(source);
+
+            })
+        })
+
+    </script>
 
 
     <script type="text/javascript">
@@ -144,9 +288,10 @@
                         category_id: category_id
                     },
                     success: function(data) {
-                        var html = '<option value="">Select Category</option>';
+                        var html = '<option value="">Select Product</option>';
                         $.each(data, function(key, v) {
-                            html += '<option value=" ' + v.id + ' "> ' + v.name + '</option>';
+                            html += '<option value=" ' + v.id + ' "> ' + v.name +
+                                '</option>';
                         });
                         $('#product_id').html(html);
                     }
@@ -154,6 +299,4 @@
             });
         });
     </script>
-
-
 @endsection
