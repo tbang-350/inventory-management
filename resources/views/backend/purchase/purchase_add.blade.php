e7f483ced2319cc0bfea91e9e0038012ce70db12
tbang-350 <tbway350@gmail.com>
2023-03-14 18:20:56 +0300

    fixed handlebar and completed multi input

--- resources/views/backend/purchase/purchase_add.blade.php
+++ resources/views/backend/purchase/purchase_add.blade.php
@@ -86,9 +86,10 @@
                                     <div class="md-3">
                                         <label for="example-text-input" class="form-label" style="margin-top: 43px">
                                         </label>
-                                        
 
-                                        <i class="btn btn-secondary btn-rounded waves-effect waves-light fas fa-plus-circle addeventmore">
+
+                                        <i
+                                            class="btn btn-secondary btn-rounded waves-effect waves-light fas fa-plus-circle addeventmore">
                                             Add More
                                         </i>
                                     </div>
@@ -164,94 +165,156 @@
     </div>
 
 
-    <script type="text/x-handlebars-template" id="document-template">
-        
-        <tr class="delete_add_more_item" id="delete_add_more_item">
 
+
+
+
+    <<script type="text/x-handlebars-template" id="document-template">
+        <tr class="delete_add_more_item" id="delete_add_more_item">
             <input type="hidden" name="date[]" value="@{{date}}">
             <input type="hidden" name="purchase_no[]" value="@{{purchase_no}}">
             <input type="hidden" name="supplier_id[]" value="@{{supplier_id}}">
-
             <td>
                 <input type="hidden" name="category_id[]" value="@{{category_id}}">
                 @{{category_name}}
             </td>
-
             <td>
                 <input type="hidden" name="product_id[]" value="@{{product_id}}">
                 @{{product_name}}
             </td>
-
             <td>
                 <input type="number" min="1" class="form-control buying_qty text-right" name="buying_qty[]" value="">
             </td>
-
             <td>
                 <input type="number" class="form-control unit_price text-right" name="unit_price[]" value="">
             </td>
-
             <td>
                 <input type="text" class="form-control" name="description[]" value="">
             </td>
-
             <td>
                 <input type="number" class="form-control buying_price text-right" name="buying_price[]" value="0" readonly>
             </td>
-
             <td>
                 <i class="btn btn-danger btn-sm fas fa-window-close removeeventmore"></i>
             </td>
-
         </tr>
-
     </script>
 
 
-    <script type="text/javascript">
 
-        $(document).ready(function(){
-            $(document).on("click",".addeventmore",function(){
+
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(document).on("click", ".addeventmore", function() {
 
                 var date = $('#date').val();
                 var purchase_no = $('#purchase_no').val();
                 var supplier_id = $('#supplier_id').val();
                 var category_id = $('#category_id').val();
-                var category_name = $('#category_name').find('option:selected').text();
+                var category_name = $('#category_id').find('option:selected').text();
                 var product_id = $('#product_id').val();
-                var product_name = $('#product_name').find('option:selected').text();
+                var product_name = $('#product_id').find('option:selected').text();
 
-                if(date == ''){
-                    $.notify("Date not set" , {globalPosition: 'top-right' ,className: 'error'});
+                if (date == '') {
+                    $.notify("Date not set", {
+                        globalPosition: 'top-right',
+                        className: 'error'
+                    });
                     return false;
                 }
 
-                if(purchase_no == ''){
-                    $.notify("Purchase_no  not set" , {globalPosition: 'top-right' ,className: 'error'});
+                if (purchase_no == '') {
+                    $.notify("Purchase_no  not set", {
+                        globalPosition: 'top-right',
+                        className: 'error'
+                    });
                     return false;
                 }
 
-                if(supplier_id == ''){
-                    $.notify("Supplier not set" , {globalPosition: 'top-right' ,className: 'error'});
+                if (supplier_id == '') {
+                    $.notify("Supplier not set", {
+                        globalPosition: 'top-right',
+                        className: 'error'
+                    });
                     return false;
                 }
 
-                if(category_id == ''){
-                    $.notify("Category not set" , {globalPosition: 'top-right' ,className: 'error'});
+                if (category_id == '') {
+                    $.notify("Category not set", {
+                        globalPosition: 'top-right',
+                        className: 'error'
+                    });
                     return false;
                 }
 
-                if(product_id == ''){
-                    $.notify("Product not set" , {globalPosition: 'top-right' ,className: 'error'});
+                if (product_id == '') {
+                    $.notify("Product not set", {
+                        globalPosition: 'top-right',
+                        className: 'error'
+                    });
                     return false;
                 }
 
-                var source = $("document-template").html();
-                var template = HandleBars.compile(source);
+                var source = $("#document-template").html();
+                var template = Handlebars.compile(source);
 
+                var data = {
+                    date: date,
+                    purchase_no: purchase_no,
+                    supplier_id: supplier_id,
+                    category_id: category_id,
+                    category_name: category_name,
+                    product_id: product_id,
+                    product_name: product_name,
+                };
+
+                var html = template(data);
+                $("#addRow").append(html);
+
+            });
+
+            $(document).on("click", ".removeeventmore", function(event) {
+                $(this).closest(".delete_add_more_item").remove();
+                totalAmountPrice();
             })
-        })
 
+            // dynamically change buying_price on change of buying_qty and unit_price
+            $(document).on('keyup click','.unit_price,.buying_price', function(){
+                var unit_price = $(this).closest("tr").find("input.unit_price").val();
+                var qty = $(this).closest("tr").find("input.buying_qty").val();
+                var total = unit_price * qty;
+                $(this).closest("tr").find("input.buying_price").val(total);
+                totalAmountPrice();
+            });
+
+            //Written by AI , Chatgpt to be specific
+            // $(document).on('keyup click', '.buying_qty,.unit_price', function() {
+            //     var row = $(this).closest('tr');
+            //     var buying_qty = row.find('.buying_qty').val();
+            //     var unit_price = row.find('.unit_price').val();
+            //     var buying_price = (buying_qty * unit_price).toFixed(2);
+            //     row.find('.buying_price').val(buying_price);
+            // });
+
+            
+            function totalAmountPrice(){
+                var sum = 0;
+
+                $(".buying_price").each(function() {
+                    var value = $(this).val();
+                    if(!isNaN(value) && value.length != 0){
+                        sum += parseFloat(value);
+                    }
+                });
+                $('#estimated_amount').val(sum);
+            }
+
+
+        });
     </script>
+
+
 
 
     <script type="text/javascript">
